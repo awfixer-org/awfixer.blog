fba811aff8132547d362ddc83b445e0019d4bcd7
awfixer <awfixer@pm.me>
2025-01-31 18:59:26 -0800

    zsh did it for me

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,30 +5,40 @@ import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 import { siteConfig } from './src/data/site.config'
 
+import cloudflare from '@astrojs/cloudflare';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.site,
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				experimentalThemes: {
-					light: 'vitesse-light',
-					dark: 'material-theme-palenight',
-				  },
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind()
-	]
+  site: siteConfig.site,
+
+  markdown: {
+      remarkPlugins: [remarkReadingTime],
+      drafts: true,
+      shikiConfig: {
+          theme: 'material-theme-palenight',
+          wrap: true
+      }
+    },
+
+  integrations: [
+      mdx({
+          syntaxHighlight: 'shiki',
+          shikiConfig: {
+              experimentalThemes: {
+                  light: 'vitesse-light',
+                  dark: 'material-theme-palenight',
+                },
+              wrap: true
+          },
+          drafts: true
+      }),
+      sitemap(),
+      tailwind()
+    ],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 })
