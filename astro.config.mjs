cc75d9c16457ed130d7587d3a0eaa9bfe8ffea9c
awfixer <awfixer@pm.me>
2025-01-31 19:13:55 -0800

    zsh did it for me

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,39 +5,38 @@ import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 import { siteConfig } from './src/data/site.config'
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 
-import node from '@astrojs/node';
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-  site: siteConfig.site,
+	site: siteConfig.site,
 
-  markdown: {
-      remarkPlugins: [remarkReadingTime],
-      drafts: true,
-      shikiConfig: {
-          theme: 'material-theme-palenight',
-          wrap: true
-      }
-    },
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
 
-  integrations: [
-      mdx({
-          syntaxHighlight: 'shiki',
-          shikiConfig: {
-              experimentalThemes: {
-                  light: 'vitesse-light',
-                  dark: 'material-theme-palenight',
-                },
-              wrap: true
-          },
-          drafts: true
-      }),
-      sitemap(),
-      tailwind()
-    ],
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight'
+				},
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	],
 
-  adapter: cloudflare()
-  })
+	adapter: cloudflare()
 })
