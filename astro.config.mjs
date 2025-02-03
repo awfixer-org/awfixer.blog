52f6dd459449ac17d9f033ba88d98016ac650c29
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-02 20:06:52 -0800

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,37 +7,37 @@ import { siteConfig } from './src/data/site.config'
 
 import react from '@astrojs/react';
 
-// import cloudflare from '@astrojs/cloudflare'
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.site,
+  site: siteConfig.site,
 
-    markdown: {
-        remarkPlugins: [remarkReadingTime],
-        drafts: true,
-        shikiConfig: {
-            theme: 'material-theme-palenight',
-            wrap: true
-        }
-    },
+  markdown: {
+      remarkPlugins: [remarkReadingTime],
+      drafts: true,
+      shikiConfig: {
+          theme: 'material-theme-palenight',
+          wrap: true
+      }
+  },
 
-    integrations: [mdx({
-        syntaxHighlight: 'shiki',
-        shikiConfig: {
-            experimentalThemes: {
-                light: 'vitesse-light',
-                dark: 'material-theme-palenight'
-            },
-            wrap: true
-        },
-        drafts: true
-		}), sitemap(), tailwind(), react()],
-    // output: 'server',
-    // adapter: cloudflare(),
-    vite: {
-        ssr: {
-            external: ['node:path']
-        }
-    }
+  integrations: [mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+          experimentalThemes: {
+              light: 'vitesse-light',
+              dark: 'material-theme-palenight'
+          },
+          wrap: true
+      },
+      drafts: true
+      }), sitemap(), tailwind(), react()],
+  vite: {
+      ssr: {
+          external: ['node:path']
+      }
+  },
+  output: 'server',
+  adapter: cloudflare()
 })
