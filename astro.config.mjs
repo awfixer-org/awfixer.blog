879f9fc48503709af283c520931c56a14fd6fe6b
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-04 01:12:18 -0800

    Message Api Workaround

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,61 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwindcss from '@tailwindcss/vite'
-import { remarkReadingTime } from './src/utils/readTime.ts'
-import { siteConfig } from './src/data/site.config'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { remarkReadingTime } from "./src/utils/readTime.ts";
+import { siteConfig } from "./src/data/site.config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: siteConfig.site,
+    site: siteConfig.site,
 
-  markdown: {
-      remarkPlugins: [remarkReadingTime],
-      drafts: true,
-      shikiConfig: {
-          theme: 'material-theme-palenight',
-          wrap: true
-      }
-  },
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+        drafts: true,
+        shikiConfig: {
+            theme: "material-theme-palenight",
+            wrap: true,
+        },
+    },
 
-  integrations: [mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: {
-          experimentalThemes: {
-              light: 'vitesse-light',
-              dark: 'material-theme-palenight'
-          },
-          wrap: true
-      },
-      drafts: true
-      }), sitemap(), react()],
-  vite: {
-    plugins: [tailwindcss()],
-      ssr: {
-          external: ['node:path', 'node:stream', 'node:util', 'node:buffer', 'node:fs', 'node:os']
-      }
-  },
-  output: 'server',
-  adapter: cloudflare()
-})
+    integrations: [
+        mdx({
+            syntaxHighlight: "shiki",
+            shikiConfig: {
+                experimentalThemes: {
+                    light: "vitesse-light",
+                    dark: "material-theme-palenight",
+                },
+                wrap: true,
+            },
+            drafts: true,
+        }),
+        sitemap(),
+        react(),
+    ],
+    vite: {
+        plugins: [tailwindcss()],
+        ssr: {
+            external: [
+                "node:path",
+                "node:stream",
+                "node:util",
+                "node:buffer",
+                "node:fs",
+                "node:os",
+            ],
+        },
+        resolve: {
+            alias: import.meta.env.PROD && {
+                "react-dom/server": "react-dom/server.edge",
+            },
+        },
+    },
+    output: "server",
+    adapter: cloudflare(),
+});
+
