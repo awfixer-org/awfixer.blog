17d8e2b61b5b80c277195b8a900e684817b238d1
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-04 23:25:28 -0800

    update tokens

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || 'master'
 
 export default defineConfig({
 	branch,
-	clientId: process.env.TINA_TOKEN, // Get this from tina.io
-	token: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, // Get this from tina.io
+	clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, // Get this from tina.io
+	token: process.env.TINA_TOKEN, // Get this from tina.io
 
 	build: {
 		outputFolder: 'admin',
